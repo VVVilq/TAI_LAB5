16619522ae12e49c52ce65ea63934ed993e5660e
Mateusz Wilk <mateuszwilk997@gmail.com>
2019-04-09 15:37:50 +0200

    learning typescript

---
+++ complex.js
@@ -0,0 +1,27 @@
+"use strict";
+class Complex {
+    constructor(re, im) {
+        this.re = re;
+        this.im = im;
+    }
+    add(complex) {
+        return new Complex(this.re + complex.re, this.im + complex.im);
+    }
+    sub(complex) {
+        return new Complex(this.re - complex.re, this.im - complex.im);
+    }
+    abs() {
+        return Math.sqrt(Math.pow(this.re, 2) + (Math.pow(this.im, 2)));
+    }
+    toString() {
+        return this.re.toString() + "+" + this.im.toString() + "i";
+    }
+}
+let complex1 = new Complex(3, 4);
+let complex2 = new Complex(2, 3);
+let complexResult1 = complex1.add(complex2);
+console.log(complexResult1.toString());
+let complexResult2 = complexResult1.sub(complex1);
+console.log(complexResult2.toString());
+let complexResult3 = complexResult2.abs();
+console.log(complexResult3);
